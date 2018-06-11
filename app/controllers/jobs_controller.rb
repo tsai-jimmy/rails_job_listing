ee5a4d57905c8719f2e928cfbde2cc462aeb249a
tsai-jimmy <qaz3182@gmail.com>
2018-06-11 21:09:02 +0800

    add validation to Job name & description

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,34 +2,44 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+  
   def show
   	@job = Job.find(params[:id])
   end
+
   def new
   	@job = Job.new
   end
+
   def create
   	@job = Job.new(job_params)
-  	@job.save
-  	redirect_to jobs_path
+  	if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+  	end
   end
+
   def edit
   	@job = Job.find(params[:id])
   end
+
   def update
   	@job = Job.find(params[:id])
   	@job.update(job_params)
   	redirect_to jobs_path, notice: "Update Success"
   end
+
   def destroy
 	@job = Job.find(params[:id])
 	@job.destroy
 	flash[:alert] = "Group deleted"
 	redirect_to jobs_path
   end
+
   private
   def job_params
-  	params.require(:job).permit(:tite, :description)
+  	params.require(:job).permit(:title, :description)
   end
 
 end
