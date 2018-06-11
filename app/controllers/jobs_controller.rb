dd4b6251358f744395ce91d2c780cdda59b413b8
tsai-jimmy <qaz3182@gmail.com>
2018-06-11 19:31:06 +0800

    implement jobs#create & show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,23 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+  def show
+  	@job = Job.find(params[:id])
+  end
   def new
   	@job = Job.new
   end
+  def create
+  	@job = Job.new(job_params)
+  	@job.save
+  	redirect_to jobs_path
+  end
+  def edite
+  end
+
+  private
+  def job_params
+  	params.require(:job).permit(:tite, :description)
+  end
+
 end
