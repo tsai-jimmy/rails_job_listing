819d42d8596685bc3506d44e7f35a8acdab9daa2
tsai-jimmy <qaz3182@gmail.com>
2018-06-19 12:12:56 +0800

    refactor job list second by use helper method

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,9 +1,12 @@
 module JobsHelper
-	def render_job_status(job)
-	  if job.is_hidden
-	    content_tag(:span, "", :class => "fa fa-lock")
-	  else
-	    content_tag(:span, "", :class => "fa fa-globe")
-	  end
-	end
+  def render_job_status(job)
+    if job.is_hidden
+      content_tag(:span, "", :class => "fa fa-lock")
+    else
+      content_tag(:span, "", :class => "fa fa-globe")
+    end
+  end
+  def can_operator?(job,who)
+    who == job.user || who.admin?
+  end
 end
